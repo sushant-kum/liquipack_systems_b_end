66da5cc796246dd95d0f62e1b4c0278cea6e3f6f
Sushant Kumar <sushant.kum96@gmail.com>
2019-06-25 10:55:07 +0530

    API /api/profile/:user_id

--- configs/apps.config.js
+++ configs/apps.config.js
@@ -1,7 +1,8 @@
 let app_names = {
     login: "login",
     token: "token",
-    system_users: "system-users"
+    system_users: "system-users",
+    settings_profile: "settings-profile"
 };
 let permissions = {
     read: "read",
@@ -10,11 +11,13 @@ let permissions = {
 let compulsory_app_permissions = {};
 compulsory_app_permissions[app_names.login] = [permissions.read, permissions.write];
 compulsory_app_permissions[app_names.token] = [permissions.read, permissions.write];
+compulsory_app_permissions[app_names.settings_profile] = [permissions.read, permissions.write];
 
 exports.app_names = app_names;
 exports.permissions = permissions;
 exports.compulsory_apps = [
     app_names.login,
-    app_names.token
+    app_names.token,
+    app_names.settings_profile
 ];
 exports.compulsory_app_permissions = compulsory_app_permissions;
--- routers/api.router.js
+++ routers/api.router.js
@@ -5,11 +5,12 @@ const base_path = path.dirname(require.main.filename);
 
 let AuthController = require(base_path + '/controllers/auth.controller.js');
 
-let _get__login = require(base_path + '/handlers/GET/api/login/get__login.handler');
-let _get__login__token = require(base_path + '/handlers/GET/api/login/token/get__login__token.handler');
-let _put__login__refreshtoken = require(base_path + '/handlers/PUT/api/login/refreshtoken/put__login__refreshtoken.handler');
+let _get__login = require(base_path + '/handlers/GET/api/login/get__login');
+let _get__login__token = require(base_path + '/handlers/GET/api/login/token/get__login__token');
+// let _put__login__refreshtoken = require(base_path + '/handlers/PUT/api/login/refreshtoken/put__login__refreshtoken');
+let _get__profile = require(base_path + '/handlers/GET/api/profile/:user_id/get__profile__:user_id');
 let _get__users = require(base_path + '/handlers/GET/api/users/get__users');
-let _post__user = require(base_path + '/handlers/POST/api/users/post__users');
+let _post__users = require(base_path + '/handlers/POST/api/users/post__users');
 let _get__users__user_id = require(base_path + '/handlers/GET/api/users/:user_id/get__users__:user_id');
 let _put__users__user_id = require(base_path + '/handlers/PUT/api/users/:user_id/put__users__:user_id');
 let _delete__users__user_id = require(base_path + '/handlers/DELETE/api/users/:user_id/delete__users__:user_id');
@@ -26,10 +27,12 @@ router.route('/api/login/token').get(AuthController.isJWTAuthenticated, _get__lo
 // router.route('/api/login/refreshtoken').put(_put__login__refreshtoken.handler);
 
 router.route('/api/users').get(AuthController.isJWTAuthenticated, _get__users.handler);
-router.route('/api/users').post(AuthController.isJWTAuthenticated, _post__user.handler);
+router.route('/api/users').post(AuthController.isJWTAuthenticated, _post__users.handler);
 
 router.route('/api/users/:user_id').get(AuthController.isJWTAuthenticated, _get__users__user_id.handler);
 router.route('/api/users/:user_id').put(AuthController.isJWTAuthenticated, _put__users__user_id.handler);
 router.route('/api/users/:user_id').delete(AuthController.isJWTAuthenticated, _delete__users__user_id.handler);
+
+router.route('/api/profile/:user_id').get(AuthController.isJWTAuthenticated, _get__profile.handler);
 
 module.exports = router;
