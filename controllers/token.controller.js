361d45a80e1257266814914485e487af20650fe4
Sushant Kumar <sushant.kum96@gmail.com>
2019-08-19 19:24:31 +0530

    controllers

--- controllers/token.controller.js
+++ controllers/token.controller.js
@@ -4,49 +4,49 @@ const base_path = path.dirname(require.main.filename);
 Token = require(base_path + '/models/token.model');
 
 exports.findOne = function (query, callback) {
-    Token.findOne(query, function (error, token) {
-        if (error) {
-            callback(error);
-        }
-        else {
-            callback(null, token);
-        }
-    });
+  Token.findOne(query, function (error, token) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(null, token);
+    }
+  });
 };
 
 exports.find = function (query, callback) {
-    Token.find(query, function (error, tokens) {
-        if (error) {
-            callback(error);
-        }
-        else {
-            callback(null, tokens);
-        }
-    });
+  Token.find(query, function (error, tokens) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(null, tokens);
+    }
+  });
 };
 
 exports.index = function (callback) {
-    Token.get(function (error, tokens) {
-        if (error)
-            callback(error);
-        else
-            callback(null, tokens);
-    });
+  Token.get(function (error, tokens) {
+    if (error)
+      callback(error);
+    else
+      callback(null, tokens);
+  });
 };
 
 exports.new = function (new_token, callback) {
-    let token = new Token();
-    token.username = new_token.username;
-    token.token = new_token.token;
-    token.uuid = new_token.uuid;
-    token.save(function (error) {
-        if (error) {
-            callback(error);
-        }
-        else {
-            callback(null, token);
-        }
-    });
+  let token = new Token();
+  token.username = new_token.username;
+  token.token = new_token.token;
+  token.uuid = new_token.uuid;
+  token.save(function (error) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(null, token);
+    }
+  });
 };
 
 // exports.view = function (user_id, callback) {
@@ -62,58 +62,58 @@ exports.new = function (new_token, callback) {
 
 
 exports.update = function (token_id, updated_token, callback) {
-    Token.findById(token_id, function (error, token) {
+  Token.findById(token_id, function (error, token) {
+    if (error) {
+      res.json({
+        status: "error",
+        message: error,
+      });
+    }
+    else {
+      token.token = updated_token.token;
+      token.last_updated_timestamp = Date.now();
+      token.uuid = updated_token.uuid;
+      token.save(function (error) {
         if (error) {
-            res.json({
-                status: "error",
-                message: error,
-            });
+          callback(error);
         }
         else {
-            token.token = updated_token.token;
-            token.last_updated_timestamp = Date.now();
-            token.uuid = updated_token.uuid;
-            token.save(function (error) {
-                if (error) {
-                    callback(error);
-                }
-                else {
-                    callback(null, token);
-                }
-
-            });
+          callback(null, token);
         }
-    });
+
+      });
+    }
+  });
 };
 
 exports.make_inactive = function (token_id, callback) {
-    Token.findById(token_id, function (error, token) {
+  Token.findById(token_id, function (error, token) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      token.is_active = false;
+      token.save(function (error) {
         if (error) {
-            callback(error);
+          callback(error);
         }
         else {
-            token.is_active = false;
-            token.save(function (error) {
-                if (error) {
-                    callback(error);
-                }
-                else {
-                    callback(null, token);
-                }
-            });
+          callback(null, token);
         }
-    });
+      });
+    }
+  });
 };
 
 exports.delete = function (token_id, callback) {
-    Token.deleteOne({
-        _id: token_id
-    }, function (error, token) {
-        if (error) {
-            callback(error);
-        }
-        else {
-            callback(null, token);
-        }
-    });
+  Token.deleteOne({
+    _id: token_id
+  }, function (error, token) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(null, token);
+    }
+  });
 };
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,146 +1,146 @@
 User = require('../models/user.model');
 
 exports.findOne = function (query, callback) {
-    User.findOne(query, function (error, user) {
-        if (error) {
-            callback(error);
-        }
-        else {
-            callback(null, user);
-        }
-    });
+  User.findOne(query, function (error, user) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(null, user);
+    }
+  });
 };
 
 exports.index = function (callback) {
-    User.get(function (error, users) {
-        if (error)
-            callback(error);
-        else
-            callback(null, users);
-    });
+  User.get(function (error, users) {
+    if (error)
+      callback(error);
+    else
+      callback(null, users);
+  });
 };
 
 exports.new = function (new_user, callback) {
-    let user = new User();
-    user.username = new_user.username;
-    user.password_hash = new_user.password_hash;
-    user.app_permissions = new_user.app_permissions ? new_user.app_permissions : [];
-    user.name = new_user.name;
-    user.gender = new_user.gender;
-    user.email = new_user.email;
-    user.phone = new_user.phone;
-    user.save(function (error) {
-        if (error) {
-            callback(error);
-        }
-        else {
-            callback(null, user);
-        }
-    });
+  let user = new User();
+  user.username = new_user.username;
+  user.password_hash = new_user.password_hash;
+  user.app_permissions = new_user.app_permissions ? new_user.app_permissions : [];
+  user.name = new_user.name;
+  user.gender = new_user.gender;
+  user.email = new_user.email;
+  user.phone = new_user.phone;
+  user.save(function (error) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(null, user);
+    }
+  });
 };
 
 exports.view = function (user_id, callback) {
-    User.findById(user_id, function (error, user) {
-        if (error) {
-            callback(error);
-        }
-        else {
-            callback(null, user);
-        }
-    });
+  User.findById(user_id, function (error, user) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(null, user);
+    }
+  });
 };
 
 
 exports.update = function (user_id, update_user, callback) {
-    User.findById(user_id, function (error, user) {
+  User.findById(user_id, function (error, user) {
+    if (error) {
+      res.json({
+        status: "error",
+        message: error,
+      });
+    }
+    else {
+      user.password_hash = update_user.password_hash;
+      user.app_permissions = update_user.app_permissions;
+      user.name = update_user.name;
+      user.gender = update_user.gender;
+      user.email = update_user.email;
+      user.phone = update_user.phone;
+      user.save(function (error) {
         if (error) {
-            res.json({
-                status: "error",
-                message: error,
-            });
+          callback(error);
         }
         else {
-            user.password_hash = update_user.password_hash;
-            user.app_permissions = update_user.app_permissions;
-            user.name = update_user.name;
-            user.gender = update_user.gender;
-            user.email = update_user.email;
-            user.phone = update_user.phone;
-            user.save(function (error) {
-                if (error) {
-                    callback(error);
-                }
-                else {
-                    callback(null, user);
-                }
-
-            });
+          callback(null, user);
         }
-    });
+
+      });
+    }
+  });
 };
 
 exports.make_inactive = function (user_id, callback) {
-    User.findById(user_id, function (error, user) {
+  User.findById(user_id, function (error, user) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      user.is_active = false;
+      user.save(function (error) {
         if (error) {
-            callback(error);
+          callback(error);
         }
         else {
-            user.is_active = false;
-            user.save(function (error) {
-                if (error) {
-                    callback(error);
-                }
-                else {
-                    callback(null, user);
-                }
-            });
+          callback(null, user);
         }
-    });
+      });
+    }
+  });
 };
 
 exports.make_active = function (user_id, callback) {
   User.findById(user_id, function (error, user) {
-      if (error) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      user.is_active = true;
+      user.save(function (error) {
+        if (error) {
           callback(error);
-      }
-      else {
-          user.is_active = true;
-          user.save(function (error) {
-              if (error) {
-                  callback(error);
-              }
-              else {
-                  callback(null, user);
-              }
-          });
-      }
+        }
+        else {
+          callback(null, user);
+        }
+      });
+    }
   });
 };
 
 exports.delete = function (user_id, callback) {
-    User.deleteOne({
-        _id: user_id
-    }, function (error, user) {
-        if (error) {
-            callback(error);
-        }
-        else {
-            callback(null, user);
-        }
-    });
+  User.deleteOne({
+    _id: user_id
+  }, function (error, user) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(null, user);
+    }
+  });
 };
 
 exports.checkCredentials = function (username, password_hash, callback) {
-    User.findOne({
-        username: username,
-        password_hash: password_hash,
-        is_active: true
-    }, 'app_permissions', function (error, user) {
-        if (error) {
-            callback(error);
-        }
-        else {
-            callback(null, user);
-        }
-    });
+  User.findOne({
+    username: username,
+    password_hash: password_hash,
+    is_active: true
+  }, 'app_permissions', function (error, user) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(null, user);
+    }
+  });
 };
