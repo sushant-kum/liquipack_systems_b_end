ad57539d90dc791af80086a9d1d2c1a1ef2a3f6d
Sushant Kumar <sushant.kum96@gmail.com>
2019-08-19 19:29:50 +0530

    /

--- index.js
+++ index.js
@@ -15,45 +15,45 @@ let logger = require(base_path + '/helpers/logger.helper');
 const port = process.env.PORT || 8080;
 
 mongoose.connect(db_connection.connection_string, {
-    useCreateIndex: true,
-    useNewUrlParser: true
+  useCreateIndex: true,
+  useNewUrlParser: true
 });
 let db = mongoose.connection;
 db.on('error', (error) => {
-    logger.error(error);
-    logger.error("Error connecting to db_config:", db_connection.config, "connection_string: ", db_connection.connection_string);
+  logger.error(error);
+  logger.error("Error connecting to db_config:", db_connection.config, "connection_string: ", db_connection.connection_string);
 });
 db.on('connected', () => {
-    logger.log('Connected to db_config:', db_connection.config, { log_to_console: true });
-    let app = express();
+  logger.log('Connected to db_config:', db_connection.config, { log_to_console: true });
+  let app = express();
 
-    app.use(passport.initialize());
+  app.use(passport.initialize());
 
-    // for parsing application/xwww-form-urlencoded
-    app.use(body_parser.urlencoded({
-        extended: true
-    }));
-    // for parsing application/json
-    app.use(body_parser.json());
-    // for parsing multipart/form-data
-    app.use(multer.array());
+  // for parsing application/xwww-form-urlencoded
+  app.use(body_parser.urlencoded({
+    extended: true
+  }));
+  // for parsing application/json
+  app.use(body_parser.json());
+  // for parsing multipart/form-data
+  app.use(multer.array());
 
-    app.use('/assets', express.static('assets'));
-    // app.get('/', (req, res) => res.send('liquipack_systems APIs'));
-    app.use('/', api_router);
-    app.use('/api-doc', apidoc_router);
+  app.use('/assets', express.static('assets'));
+  // app.get('/', (req, res) => res.send('liquipack_systems APIs'));
+  app.use('/', api_router);
+  app.use('/api-doc', apidoc_router);
 
-    app.listen(port, function () {
-        logger.log("Started liquipack_systems API service on port " + port, { log_to_console: true });
-    });
+  app.listen(port, function () {
+    logger.log("Started liquipack_systems API service on port " + port, { log_to_console: true });
+  });
 });
 db.on('disconnected', () => {
-    logger.error("Disconnected from db_config:", db_connection.config, "connection_string: ", db_connection.connection_string, { log_to_console: true });
+  logger.error("Disconnected from db_config:", db_connection.config, "connection_string: ", db_connection.connection_string, { log_to_console: true });
 });
 
-process.on('SIGINT', function(){
-    db.close(function(){
-        logger.error("Mongoose default connection is disconnected due to application termination", { log_to_console: true });
-        process.exit(0);
-    });
+process.on('SIGINT', function () {
+  db.close(function () {
+    logger.error("Mongoose default connection is disconnected due to application termination", { log_to_console: true });
+    process.exit(0);
+  });
 });
