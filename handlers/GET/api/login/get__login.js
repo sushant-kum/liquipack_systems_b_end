fe67349f63601d78ae32a36fd241bbefef43431a
Sushant Kumar <sushant.kum96@gmail.com>
2019-08-19 19:25:36 +0530

    handlers/GET

--- handlers/GET/api/login/get__login.js
+++ handlers/GET/api/login/get__login.js
@@ -12,80 +12,80 @@ let check_app_permission = require(base_path + '/helpers/check_app_permission.he
 
 
 exports.handler = function (req, res) {
-    try {
-        check_app_permission(app_config.app_names.login, app_config.permissions.read, req.user.username, function (error, is_permitted = null) {
-            if (error) {
-                logger.error(error, { log_to_console: true });
-                return res.status(500).json({
-                    status: "error",
-                    message: error,
-                    user_id: req.user._id,
-                    token: req.user.token
-                });
-            }
-            else {
-                if (is_permitted) {
-                    const uuid = uuidv4();
-                    const jwt_payload = {
-                        username: req.user.username,
-                        uuid: uuid
-                    }
-                    const token = jwt.sign(jwt_payload, jwt_config.token_secret, { expiresIn: jwt_config.token_life });
-                    let new_token = {
-                        username: req.user.username,
-                        token: token,
-                        uuid: uuid
-                    };
-                    TokenController.new(new_token, function (error, token = null) {
-                        if (error) {
-                            logger.error(error, { log_to_console: true });
-                            return res.status(500).json({
-                                status: "error",
-                                message: error,
-                                user_id: req.user._id,
-                                token: req.user.token
-                            });
-                        }
-                        else {
-                            return res.json({
-                                status: "success",
-                                message: "Login Successful",
-                                data: {
-                                    app_permissions: req.user.app_permissions
-                                },
-                                user_id: req.user._id,
-                                token: new_token.token
-                            });
-                        }
-                    });
-                    // return res.json({
-                    //     status: "success",
-                    //     message: "Login Successful",
-                    //     data: {
-                    //         user_id: req.user._id,
-                    //         app_permissions: req.user.app_permissions
-                    //     },
-                    //     token: token
-                    // });
-                }
-                else {
-                    return res.status(403).json({
-                        status: "access_denied",
-                        message: "Access to this operation is denied",
-                        user_id: req.user._id,
-                        token: req.user.token
-                    });
-                }
-            }
-        });
-    }
-    catch (error) {
+  try {
+    check_app_permission(app_config.app_names.login, app_config.permissions.read, req.user.username, function (error, is_permitted = null) {
+      if (error) {
         logger.error(error, { log_to_console: true });
         return res.status(500).json({
-            status: "error",
-            message: error,
+          status: "error",
+          message: error,
+          user_id: req.user._id,
+          token: req.user.token
+        });
+      }
+      else {
+        if (is_permitted) {
+          const uuid = uuidv4();
+          const jwt_payload = {
+            username: req.user.username,
+            uuid: uuid
+          }
+          const token = jwt.sign(jwt_payload, jwt_config.token_secret, { expiresIn: jwt_config.token_life });
+          let new_token = {
+            username: req.user.username,
+            token: token,
+            uuid: uuid
+          };
+          TokenController.new(new_token, function (error, token = null) {
+            if (error) {
+              logger.error(error, { log_to_console: true });
+              return res.status(500).json({
+                status: "error",
+                message: error,
+                user_id: req.user._id,
+                token: req.user.token
+              });
+            }
+            else {
+              return res.json({
+                status: "success",
+                message: "Login Successful",
+                data: {
+                  app_permissions: req.user.app_permissions
+                },
+                user_id: req.user._id,
+                token: new_token.token
+              });
+            }
+          });
+          // return res.json({
+          //     status: "success",
+          //     message: "Login Successful",
+          //     data: {
+          //         user_id: req.user._id,
+          //         app_permissions: req.user.app_permissions
+          //     },
+          //     token: token
+          // });
+        }
+        else {
+          return res.status(403).json({
+            status: "access_denied",
+            message: "Access to this operation is denied",
             user_id: req.user._id,
             token: req.user.token
-        });
-    }
+          });
+        }
+      }
+    });
+  }
+  catch (error) {
+    logger.error(error, { log_to_console: true });
+    return res.status(500).json({
+      status: "error",
+      message: error,
+      user_id: req.user._id,
+      token: req.user.token
+    });
+  }
 };
