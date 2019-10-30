790220387d4a8c4f622fcc68e5a925d9e9345e3a
Sushant Kumar <sushant.kum96@gmail.com>
2019-10-30 12:08:27 +0530

    added CORS for https://liquipack.cf

---
+++ configs/cors.config.js
@@ -0,0 +1,15 @@
+const whitelist = ['https://liquipack.sushantk.com', 'https://liquipack.cf'];
+
+exports.options = {
+  origin: function(origin, callback) {
+    if (
+      (process.env.NODE_ENV === 'production' && whitelist.includes(origin)) ||
+      process.env.NODE_ENV !== 'production'
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error(`Not allowed by CORS. Origin: ${origin}`));
+    }
+  },
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
